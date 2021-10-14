6246448b8bfdd8bbff4ce6d4338c2733d0e4ead1
Patrick Moores <patrickmoores@Patricks-MacBook-Air.local>
2021-10-14 18:44:08 -0400

    Update after Del 2 complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,8 @@
+# DELIVERABLE 1
 # Import dplyr
 library(dplyr)
 
-# Read in a CSV file as demo_table
+# Read in a CSV file as Mecha_Car_table
 MechaCar_mpg_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
 # Generate multiple linear regression model with MechaCar_mpg_table with mpg as depen var
@@ -9,3 +10,18 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Use summary() to determine the p-values and the r-squared values
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg_table))
+
+
+# DELIVERABLE 2
+# Read in the CSV file as Suspension_Coil_table
+Suspension_Coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Import tidyverse
+library(tidyverse)
+
+# Create a total_summary DataFrame
+total_summary <- Suspension_Coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+# Create a lot_summary DataFrame 
+lot_summary <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
--- README.md
+++ README.md
@@ -21,3 +21,7 @@ Design a statistical study to compare vehicle performance of the MechaCar vehicl
 - Which variables/coefficients provided a non-random amount of variance to the mpg values in the dataset?
 - Is the slope of the linear model considered to be zero? Why or why not?
 Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
+
+
+## Summary Statistics on Suspension Coils
+
