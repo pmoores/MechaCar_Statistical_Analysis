f5766809f0d543a3fbc59fc2c14c57301385d4d2
Patrick Moores <patrickmoores@Patricks-MacBook-Air.local>
2021-10-14 23:39:05 -0400

    Updates to Del 3 in readme

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -30,7 +30,21 @@ lot_summary <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summari
 # DELIVERABLE 3
 # Create a sample table
 sample_coil_table <- Suspension_Coil_table %>% sample_n(50)
-
 # Perform a one-sample t-test to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 PSI
 t.test(log10(sample_coil_table$PSI),mu=mean(log10(Suspension_Coil_table$PSI)))
+
+# Create Lot 1 table
+Lot1_table <- filter(Suspension_Coil_table, Manufacturing_Lot == "Lot1")
+# Perform Lot 1 t-test
+t.test(log10(Lot1_table$PSI),mu=mean(log10(Suspension_Coil_table$PSI)))
+
+# Create Lot 2 table
+Lot2_table <- filter(Suspension_Coil_table, Manufacturing_Lot == "Lot2")
+# Perform Lot 2 t-test
+t.test(log10(Lot2_table$PSI),mu=mean(log10(Suspension_Coil_table$PSI)))
+
+# Create Lot 3 table
+Lot3_table <- filter(Suspension_Coil_table, Manufacturing_Lot == "Lot3")
+# Perform Lot 3 t-test
+t.test(log10(Lot3_table$PSI),mu=mean(log10(Suspension_Coil_table$PSI)))
 
